2e04c777ec5af77b248ffebe53d202ccdafbcba9
nada himdi <129855859+nadahimdi@users.noreply.github.com>
2023-05-19 23:35:07 +0200

    Add files via upload

---
+++ pageprinc.php
@@ -0,0 +1,415 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/principale.css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Moon+Dance&family=Open+Sans:wght@400;700;800&family=Press+Start+2P&family=Work+Sans:wght@200;300;400;600&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link rel="shortcut icon" href="logos/favicon.ico" type="image/x-icon">
+
+<link href="https://fonts.googleapis.com/css2?family=Concert+One&family=Courgette&family=Handlee&family=Merriweather+Sans:wght@600&family=Nunito&family=Playfair+Display&family=Rubik:ital,wght@0,500;1,300;1,800&family=Titan+One&family=Work+Sans:wght@200;300;600&display=swap" rel="stylesheet">
+<body>
+
+ 
+    <!-- Start Header -->
+    <header>
+      <div class="container">
+        <a href="#" class="logo">
+          <img decoding="async" src="./imgs/logo-icon-28.jpg" alt="Logo" />
+        </a>
+        <nav>
+          <i class="fas fa-bars toggle-menu"></i>
+          <ul>
+            <li><a  href="#">Home</a></li>
+            <li><a href="./influencerphp/influencer.php">Influencer</a></li>
+            <li><a href="./myadm/login.php">Admin</a></li>
+            <li><a href="./brandphp/brand.php">Brand</a></li>
+          </ul>
+          <div class="form">
+            <i class="fas fa-search"></i>
+          </div>
+        </nav>
+      </div>
+    </header>
+    <!-- End Header -->
+
+    <!-- Start Landing -->
+    <div class="landing">
+        <div class="overlay"> </div>
+        <div class="text">
+            <div class="content">
+                <h2>
+                  Welcome!<br />
+                 To the collaboration world
+                </h2>
+                <p>
+                    Bienvenue sur notre page de collaboration entre influenceurs et marques ! Nous sommes ravis de vous accueillir dans cet espace dédié à la création de partenariats réussis. Nous croyons que les collaborations entre influenceurs et marques peuvent être mutuellement bénéfiques et nous sommes ici pour vous aider à créer des projets authentiques et pertinents pour votre audience. Nous sommes impatients de travailler avec vous pour créer du contenu engageant et mémorable qui vous permettra d'atteindre vos objectifs marketing et de donner à votre public ce qu'il veut voir.
+                </p>
+              </div>
+        </div>
+       
+       
+    </div>
+
+
+    <!-- End Landing -->
+
+
+     <!-- Start who are you -->
+     <div class="articles" id="articles">
+        <h2 class="main-title">Who Are You</h2>
+        <div class="container">
+            <div class="box">
+                <img src="imgs/Colorful icons set design.jpeg" alt="" />
+                <div class="content">
+                  <h3>Hello Dear Influencer</h3>
+                  <p>Notre plateforme de partenariat vous permet de trouver des marques de qualité et de créer des collaborations efficaces pour maximiser votre influence en ligne. En utilisant notre site, vous pouvez trouver des partenariats rémunérés et non-rémunérés, des campagnes publicitaires et des collaborations à long terme pour promouvoir les produits et services que vous aimez. Rejoignez notre communauté dès maintenant pour trouver de nouvelles opportunités et développer votre carrière d'influenceur !</p>
+                </div>
+                <div class="info">
+                  <a href="./influencerphp/influencer.php">Join Us</a>
+                  
+                </div>
+              </div>
+         
+          <div class="box">
+            <img src="imgs/Presentation 3D scene.png" alt="" />
+            <div class="content">
+              <h3>Hello Dear Admin</h3>
+              <p>Notre plateforme de partenariat vous permet de trouver les influenceurs les mieux adaptés pour votre entreprise et de créer des collaborations efficaces pour atteindre votre public cible. En utilisant notre site, vous pouvez augmenter votre visibilité en ligne et promouvoir vos produits et services grâce à notre communauté d'influenceurs de qualité. Rejoignez notre plateforme dès maintenant pour atteindre vos objectifs de communication digitale et trouver de nouvelles opportunités et  développer votre carrière d'influenceur !</p>
+            </div>
+            <div class="info">
+              <a href="">Join Us</a>
+              
+            </div>
+          </div>
+          <div class="box">
+            <img src="imgs/Nike Air Jordan 1 x High End Brands - 3D Concept.jpeg" alt="" />
+            <div class="content">
+              <h3>Hello Dear Brand</h3>
+              <p>Notre plateforme de partenariat vous permet de trouver les influenceurs les mieux adaptés pour votre entreprise et de créer des collaborations efficaces pour atteindre votre public cible. En utilisant notre site, vous pouvez augmenter votre visibilité en ligne et promouvoir vos produits et services grâce à notre communauté d'influenceurs de qualité. Rejoignez notre plateforme dès maintenant pour atteindre vos objectifs de communication digitale et trouver de nouvelles opportunités et développer votre carrière d'influenceur !</p>
+            </div>
+            <div class="info">
+              <a href="./brandphp/brand.php">Join Us</a>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <!-- End who are you -->
+
+       <!-- Start feedback -->
+    <div class="feedback" id="feedback">
+        <h2 class="main-title">Feedbaks</h2>
+        <div class="container">
+         
+         
+          
+          <div class="box">
+            <img src="imgs/emma.jpeg" alt="" />
+            <h3>Emma chembrlin</h3>
+            <span class="title">Influenceuse</span>
+            <div class="rate">
+                <p style="margin-bottom: 10px;">18M</p>
+              <i class="filled fas fa-star"></i>
+              <i class="filled fas fa-star"></i>
+              <i class="filled fas fa-star"></i>
+              <i class="filled fas fa-star"></i>
+              <i class="filled fas fa-star"></i>
+            </div>
+            <p class="p">
+                Je voulais simplement prendre un moment pour remercier l'équipe de notre plateforme de partenariat pour leur travail exceptionnel. Grâce à leur plateforme, j'ai été en mesure de trouver des marques de qualité avec lesquelles collaborer pour promouvoir des produits et services que j'aime. La facilité d'utilisation de leur site et leur engagement envers leur communauté m'ont vraiment impressionné et je suis reconnaissant de faire partie de cette communauté. Je recommande vivement cette plateforme à tous les influenceurs qui cherchent à développer leur carrière en ligne. Merci encore à toute l'équipe !
+            </p>
+          </div>
+          <div class="box">
+            <img src="imgs/kylie.jpeg" alt="" />
+            <h3>Kawtar Bamou</h3>
+            <span class="title">Influenceuse</span>
+            <div class="rate">
+                <p style="margin-bottom: 10px;">1.8M</p>
+              <i class="filled fas fa-star"></i>
+              <i class="filled fas fa-star"></i>
+              <i class="filled fas fa-star"></i>
+              <i class="far fa-star"></i>
+              <i class="far fa-star"></i>
+            </div>
+            <p class="p">
+                Je voulais simplement exprimer ma gratitude envers notre plateforme de partenariat. Depuis que j'ai rejoint leur communauté, j'ai trouvé des partenariats qui correspondent parfaitement à mon image et mes valeurs. Leur équipe de soutien est toujours là pour répondre à mes questions et m'aider à optimiser mes collaborations. Grâce à leur site, j'ai pu maximiser mon influence en ligne tout en travaillant avec des marques de qualité. Je recommande vivement cette plateforme à tous les influenceurs qui cherchent des partenariats durables avec des marques de confiance. Merci à toute l'équipe pour leur engagement envers notre communauté !
+            </p>
+          </div>
+          <div class="box">
+            <img src="imgs/Shawn-Mendes.webp" alt="" />
+            <h3>Shawn Mendes</h3>
+            <span class="title">Createur in Middle East</span>
+            <div class="rate">
+                <p style="margin-bottom: 10px;">8M</p>
+              <i class="filled fas fa-star"></i>
+              <i class="filled fas fa-star"></i>
+              <i class="filled fas fa-star"></i>
+              <i class="far fa-star"></i>
+              <i class="far fa-star"></i>
+            </div>
+            <p class="p">
+                Je suis tellement reconnaissante envers notre plateforme de partenariat pour m'avoir aidée à développer ma carrière d'influenceuse. Grâce à leur communauté de marques de qualité, j'ai pu trouver des partenariats rémunérés et non-rémunérés qui correspondent à mon public cible et à mes centres d'intérêt. Leur site est facile à utiliser et m'a permis de connecter directement avec les marques qui cherchent à travailler avec des influenceurs comme moi. Je recommande fortement cette plateforme à tous les influenceurs qui cherchent à maximiser leur influence en ligne. Merci à toute l'équipe pour leur soutien continu !
+            </p>
+          </div>
+        </div>
+      </div>
+      <!-- End feedback -->
+
+
+       <!-- Start ourbrand -->
+    <div class="ourbrand" id="ourbrand">
+        <h2 class="main-title">Our Brands</h2>
+        <div class="container">
+         
+          <div class="box">
+            <div >
+              <img  src="imgs/jumia-jumia.png" alt="" />
+            </div>
+            <div class="info">
+                <center><a href="./influencerphp/influencer.php">Contacter</a></center>
+              
+            </div>
+          </div>
+          <div class="box">
+            <div >
+              <img src="imgs/louis-vuitton-cover-958x575.jpeg" alt="" />
+            </div>
+            <div class="info">
+                <center><a href="./influencerphp/influencer.php">Contacter</a></center>
+              
+            </div>
+          </div>
+          <div class="box">
+            <div >
+              <img src="imgs/garnier-garnier.png" alt="" />
+            </div>
+            <div class="info">
+                <center><a href="./influencerphp/influencer.php">Contacter</a></center>
+              
+            </div>
+          </div>
+          <div class="box">
+            <div >
+              <img src="imgs/cocacola-coca.png" alt="" />
+            </div>
+            <div class="info">
+                <center><a href="./influencerphp/influencer.php">Contacter</a></center>
+            
+            </div>
+          </div>
+          <div class="box">
+            <div >
+              <img src="imgs/nike-nike.jpg" alt="" />
+             
+            </div>
+            <div class="info">
+                <center><a href="./influencerphp/influencer.php">Contacter</a></center>
+           
+            </div>
+          </div>
+          <div class="box">
+            <div >
+              <img src="imgs/versace-logo-icon-gianni-versace-s-r-l-usually-referred-to-simply-as-versace-italian-luxury-fashion-company-trade-name-142175212.jpg" alt="" />
+            </div>
+            <div class="info">
+                <center><a href="./influencerphp/influencer.php">Contacter</a></center>
+            </div>
+          </div>
+        </div>
+      </div>
+     
+      <!-- End ourbrand -->
+
+          <!-- Start ourinfluencer -->
+    <div class="ourinfluencer" id="ourinfluencer">
+        <h2 class="main-title">Our Influencers</h2>
+        <div class="container">
+          <div class="box">
+            <div class="image">
+              <img src="imgs/emma.jpeg" alt="" />
+            </div>
+            <br>
+            <center><h3>Emma chembrlin</h3></center>
+            <br>
+            <center><a href="./brandphp/brand.php">Contacter</a></center>
+            <br>
+          </div>
+          <div class="box">
+            <div class="image">
+              <img src="imgs/Shawn-Mendes.webp" alt="" />
+            </div>
+            <br>
+            <center><h3>Shawn mendes</h3></center>
+            <br>
+            <center><a href="./brandphp/brand.php">Contacter</a></center>
+            <br>
+          </div>
+          <div class="box">
+            <div class="image">
+              <img src="imgs/kendall.jpeg" alt="" />
+            </div>
+            <br>
+            <center><h3>Kendall jenner</h3></center>
+            <br>
+            <center><a href="./brandphp/brand.php">Contacter</a></center>
+            <br>
+          </div>
+          <div class="box">
+            <div class="image">
+              <img src="imgs/addis.jpeg" alt="" />
+            </div>
+            <br>
+            <center><h3>addison rae</h3></center>
+            <br>
+            <center><a href="./brandphp/brand.php">Contacter</a></center>
+            <br>
+          </div>
+          <div class="box">
+            <div class="image">
+              <img src="imgs/kylie.jpeg" alt="" />
+            </div>
+            <br>
+            <center><h3>kylie jenner</h3></center>
+            <br>
+            <center><a href="./brandphp/brand.php">Contacter</a></center>
+            <br>
+          </div>
+          <div class="box">
+            <div class="image">
+              <img src="imgs/anas.jpeg" alt="" />
+            </div>
+            <br>
+            <center><h3>anas kharmn</h3></center>
+            <br>
+            <center><a href="./brandphp/brand.php">Contacter</a></center>
+            <br>
+          </div>
+        </div>
+      </div>
+      <!-- End ourinfluencer -->
+
+       <!-- Start work -->
+    <div class="work" id="work">
+        
+        <h2 class="main-title">How We Work</h2>
+        <div class="container">
+          <div class="box popular1">
+           
+            <img src="imgs/Jobriver - 3D character illustration.jpeg" alt="" />
+            <div class="price">
+              <span class="amount">1</span>
+              <h3 >CRÉEZ UN CEMPTE</h3>
+            </div>
+           <p>La première étape consiste à sélectionner si vous êtes un influenceur ou une marque, puis à vous inscrire. Nous avons hâte de vous voir rejoindre notre communauté de collaboration en ligne.
+            Inscrivez-vous maintenant pour commencer à trouver les meilleures collaborations possibles pour vous et votre entreprise !</p>
+         
+          </div>
+          <div class="box popular2">
+            
+            
+            <img src="imgs/Instagram - Create Don't Hate Official Posters.jpeg" alt="" />
+            <div class="price">
+              <span class="amount">2</span>
+              <h3 >CONECTER ENTRE VOUS</h3>
+            </div>
+           <p>La deuxième étape consiste à communiquer avec d'autres influenceurs et marques pour établir des partenariats. Notre plateforme vous permet de rechercher et de connecter directement avec des partenaires potentiels, ainsi que de négocier des accords et de suivre les performances de vos collaborations. 
+            Commencez dès maintenant à établir des partenariats et à développer votre influence en ligne avec notre plateforme de partenariat.</p>
+          
+          </div>
+          <div class="box popular3">
+            
+            <img src="imgs/User10017038 _  Freepik.jpeg" alt="" />
+            <div class="price">
+              <span class="amount">3</span>
+              <h3 >CRÉER UNE COLLABORATION</h3>
+            </div>
+            
+              <p> Maintenant que vous avez communiqué avec des influenceurs et des marques et trouvé des partenaires potentiels, la dernière étape consiste à créer une collaboration réussie. Que vous recherchiez un partenariat rémunéré ou non rémunéré, notre plateforme vous permet de créer des accords personnalisés qui répondent à vos besoins et à ceux de vos partenaires.</p>
+           
+          </div>
+        </div>
+      </div>
+      <!-- End work -->
+      <center><div><img  style="width: 700px;"   src="./imgs/CanopyLAB - Social Learning Powered by AI.png" alt=" "></div></center>
+
+   <!-- Start Footer -->
+   <div class="footer">
+    <div class="container">
+     
+      <div class="box">
+        <ul class="links">
+            
+          <li> <i class="fas fa-long-arrow-alt-right"></i><a href="./influencerphp/influencer.php">Influencer</a></li>
+          <li> <i class="fas fa-long-arrow-alt-right"></i><a href="#">Admin</a></li>
+          <li> <i class="fas fa-long-arrow-alt-right"></i><a href="./influencerphp/influencer.php">Brand</a></li>
+          <li> <i class="fas fa-long-arrow-alt-right"></i><a href="#">Home</a></li>
+         
+        </ul>
+      </div>
+
+      <div class="box">
+        <center>
+        <h3>Collaboration</h3>
+        </center>
+       
+        <ul class="social">
+          <li>
+            <a href="https://www.facebook.com/" class="facebook">
+              <i class="fab fa-facebook-f"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.twitter.com/" class="twitter">
+              <i class="fab fa-twitter"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.youtube.com/" class="youtube">
+              <i class="fab fa-youtube"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/" class="instagram">
+              <i class="fab fa-instagram"></i>
+            </a>
+          </li>
+        </ul>
+        <center>
+        <p class="text">
+            Rejoignez notre communauté de partenariats dès aujourd'hui !
+        </p></center>
+      </div>
+      </center>
+      <div class="box">
+        <div class="line">
+          <i class="fas fa-map-marker-alt fa-fw"></i>
+          <div class="info">Rabat.201.Rue-massira123</div>
+        </div>
+      
+        <div class="line">
+          <i class="fas fa-phone-volume fa-fw"></i>
+          <div class="info">
+            <span>+20123456789</span>
+            <span>+20198765432</span>
+          </div>
+        </div>
+      </div>
+     
+    </div>
+    <p class="copyright">&copy; COLLA<span>BORATION</span> All Right Reserved</p>
+  </div>
+  <!-- End Footer -->
+
+
+   
+</body>
+</html>
